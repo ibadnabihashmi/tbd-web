ec216436a409c4e04c9641947cd17b0ad845808e
ehashmi <ehashmi@etilizepak.com>
2016-11-16 18:00:23 +0500

    ...lll

--- app/components/Account/CatalogueDetail.js
+++ app/components/Account/CatalogueDetail.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Messages from '../Messages';
 import { updateCatalogue, fetchCatalogueDetails } from '../../actions/catalogue';
-import Gallery from 'react-photo-gallery';
+import Lightbox from 'react-images';
 
 class CatalogueDetail extends React.Component {
     constructor(props){
@@ -14,8 +14,16 @@ class CatalogueDetail extends React.Component {
             catalogueTags: '',
             catalogueDesc: '',
             userId:props.user._id,
-            images:[]
+            images:[],
+            lightboxIsOpen:false,
+            currentImage:0,
+            display:true
         };
+
+        this.closeLightbox = this.closeLightbox.bind(this);
+        this.gotoNext = this.gotoNext.bind(this);
+        this.gotoPrevious = this.gotoPrevious.bind(this);
+        this.triggerLightBox = this.triggerLightBox.bind(this);
     }
 
     handleChange(event) {
@@ -26,7 +34,6 @@ class CatalogueDetail extends React.Component {
         const data = this.state;
         const promise = this.props.dispatch(updateCatalogue(data));
         promise.then((res) => {
-            console.log(res);
         });
     }
 
@@ -37,13 +44,7 @@ class CatalogueDetail extends React.Component {
             res.images.map((image) => {
                imgees.push({
                    src:'http://localhost:3000'+image.source,
-                   width:50,
-                   height:50,
-                   aspectRatio:1,
-                   lightboxImage:{
-                       src: 'http://localhost:3000'+image.source,
-                       caption:image.caption
-                   }
+                   caption:image.caption
                });
             });
             this.setState({
@@ -51,38 +52,127 @@ class CatalogueDetail extends React.Component {
                 catalogueName:res.catalogue.name,
                 cataloguePrice:res.catalogue.price,
                 catalogueTags:res.catalogue.hashtags.join(' '),
-                catalogueDesc:res.catalogue.description
+                catalogueDesc:res.catalogue.description,
+                display:res.catalogue.user == this.state.userId+"909090" ? false : true
             });
-            console.log(this.state)
         });
     }
 
+    triggerLightBox(index,event){
+        event.preventDefault();
+        this.setState({
+            currentImage:index,
+            lightboxIsOpen:true
+        })
+    }
+
+    gotoPrevious(){
+        this.setState({
+            currentImage:this.state.currentImage - 1
+        });
+    }
+
+    gotoNext(){
+        this.setState({
+            currentImage:this.state.currentImage + 1
+        });
+    }
+
+    closeLightbox(){
+        this.setState({
+            currentImage:0,
+            lightboxIsOpen:false
+        });
+    }
+
+    renderGallery(){
+        let images = new Array();
+        let key = 0;
+        this.state.images.forEach((image) => {
+            var style = {
+                backgroundImage:"url('"+image.src+"')"
+            };
+            images.push(
+               <a key={key+"_a"} href="#">
+                   <div className="col-md-4 gallery-image" onClick={this.triggerLightBox.bind(this,key)} key={key} style={style}>
+                   </div>
+               </a>
+            );
+            key++;
+        });
+        return images;
+    }
+
+
+    getTags(){
+        let tags = new Array();
+        let key = 0;
+        this.state.catalogueTags.split(' ').forEach((tag) => {
+            tags.push(
+                <h4 className="tag" key={key}>
+                    #{tag}
+                </h4>
+            );
+            key++;
+        });
+
+        return tags;
+    }
+
     render(){
+        const sidePanel = this.state.display ? (
+            <div className="col-lg-5">
+                <div className="catalogue-detail">
+                    <div>
+                        <h1>{this.state.catalogueName}</h1>
+                    </div>
+                    <div>
+                        <h4>{this.state.cataloguePrice} Rs</h4>
+                    </div>
+                    <div>
+                        <h5>{this.state.catalogueDesc}</h5>
+                    </div>
+                    {this.getTags()}
+                </div>
+            </div>
+        ) : (
+            <div className="col-lg-5">
+                <div className="catalogue-detail">
+                    <div className="form-group">
+                        <input type="text" name="catalogueName" id="catalogueName" placeholder="Name" className="form-control" value={this.state.catalogueName} onChange={this.handleChange.bind(this)}/>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" name="cataloguePrice" id="cataloguePrice" placeholder="Price" className="form-control" value={this.state.cataloguePrice} onChange={this.handleChange.bind(this)}/>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" name="catalogueTags" id="catalogueTags" placeholder="Tags" className="form-control" value={this.state.catalogueTags} disabled="true" onChange={this.handleChange.bind(this)}/>
+                    </div>
+                    <div className="form-group">
+                        <textarea type="text" name="catalogueDesc" id="catalogueDesc" rows="4" placeholder="Description" className="form-control" value={this.state.catalogueDesc} onChange={this.handleChange.bind(this)}></textarea>
+                    </div>
+                    <button className="btn tbd-btn" onClick={this.saveCatalogue.bind(this)}>save</button>
+                </div>
+            </div>
+        );
         return (
             <div className="container">
                 <div className="panel">
                     <div className="panel-body">
                         <Messages messages={this.props.messages}/>
                         <div className="col-lg-7">
-                            <Gallery photos={this.state.images} />
-                        </div>
-                        <div className="col-lg-5">
-                            <div className="catalogue-detail">
-                                <div className="form-group">
-                                    <input type="text" name="catalogueName" id="catalogueName" placeholder="Name" className="form-control" value={this.state.catalogueName} onChange={this.handleChange.bind(this)}/>
-                                </div>
-                                <div className="form-group">
-                                    <input type="text" name="cataloguePrice" id="cataloguePrice" placeholder="Price" className="form-control" value={this.state.cataloguePrice} onChange={this.handleChange.bind(this)}/>
-                                </div>
-                                <div className="form-group">
-                                    <input type="text" name="catalogueTags" id="catalogueTags" placeholder="Tags" className="form-control" value={this.state.catalogueTags} disabled="true" onChange={this.handleChange.bind(this)}/>
-                                </div>
-                                <div className="form-group">
-                                    <textarea type="text" name="catalogueDesc" id="catalogueDesc" rows="4" placeholder="Description" className="form-control" value={this.state.catalogueDesc} onChange={this.handleChange.bind(this)}></textarea>
-                                </div>
-                                <button className="btn tbd-btn" onClick={this.saveCatalogue.bind(this)}>save</button>
+                            <div className="col-md-12">
+                                {this.renderGallery()}
+                                <Lightbox
+                                    images={this.state.images}
+                                    currentImage={this.state.currentImage}
+                                    isOpen={this.state.lightboxIsOpen}
+                                    onClickPrev={this.gotoPrevious}
+                                    onClickNext={this.gotoNext}
+                                    onClose={this.closeLightbox}
+                                />
                             </div>
                         </div>
+                        {sidePanel}
                     </div>
                 </div>
             </div>

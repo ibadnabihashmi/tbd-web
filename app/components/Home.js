fa344c12f9ce1a76ed436c3ee26c9a6de2c993ff
ibadnabihashmi <ibad.hashmi@yahoo.com>
2016-12-03 02:54:23 +0500

    yolo

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux'
 import Messages from './Messages';
-import { fetchFeed } from '../actions/home'
+import { fetchFeed } from '../actions/home';
+import { browserHistory } from 'react-router';
 
 class Home extends React.Component {
   constructor(props) {
@@ -13,7 +14,15 @@ class Home extends React.Component {
       from: new Date(Number(from)).toISOString(),
       to: new Date(Number(to)).toISOString(),
       catalogues: []
-    }
+    };
+    this.goToUser = this.goToUser.bind(this);
+    this.goToCatalogue= this.goToCatalogue.bind(this);
+  }
+  goToUser(name){
+    browserHistory.push(`/user/${name}`);
+  }
+  goToCatalogue(id){
+    browserHistory.push(`/getCatalogue/${id}`);
   }
   getCatalogues() {
     if(this.state.catalogues.length > 0){
@@ -21,12 +30,23 @@ class Home extends React.Component {
       let imageStyle = {
         width:'100%'
       };
+      let key = 0;
       this.state.catalogues.forEach((catalogue) => {
         cats.push(
-          <div>
-            <img src={`http://localhost:3000${catalogue.images[0]}`} style={imageStyle}/>
+          <div className="feed-element" key={`key1_${key}`}>
+            <div>
+              <span className="feed-element-name" onClick={this.goToUser.bind(this,catalogue.user.name)}>{catalogue.user.name}</span>
+              <span className="feed-element-price">${catalogue.price}</span>
+            </div>
+            <img src={`http://localhost:3000${catalogue.images[0]}`} style={imageStyle} key={`key2_${key}`}/>
+            <div>
+              <h4 onClick={this.goToCatalogue.bind(this,catalogue._id)}>{catalogue.name}</h4>
+              <p>{catalogue.description}</p>
+            </div>
+            <div></div>
           </div>
         );
+        key++;
       });
       return cats;
     }else{
@@ -48,7 +68,7 @@ class Home extends React.Component {
       <div className="container-fluid">
         <Messages messages={this.props.messages}/>
         <div className="row">
-          <div className="col-sm-6 col-sm-offset-3">
+          <div className="col-sm-6 col-sm-offset-3 panel">
             <div className="col-sm-10 col-sm-offset-1">
               {this.getCatalogues()}
             </div>

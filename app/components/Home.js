63ab80b1193f4d13d350c6f9bbfb70626eb4b95b
ehashmi <ehashmi@etilizepak.com>
2016-12-02 18:24:40 +0500

    yola

--- app/components/Home.js
+++ app/components/Home.js
@@ -11,13 +11,36 @@ class Home extends React.Component {
     this.state = {
       user: props.user,
       from: new Date(Number(from)).toISOString(),
-      to: new Date(Number(to)).toISOString()
+      to: new Date(Number(to)).toISOString(),
+      catalogues: []
+    }
+  }
+  getCatalogues() {
+    if(this.state.catalogues.length > 0){
+      let cats = [];
+      let imageStyle = {
+        width:'100%'
+      };
+      this.state.catalogues.forEach((catalogue) => {
+        cats.push(
+          <div>
+            <img src={`http://localhost:3000${catalogue.images[0]}`} style={imageStyle}/>
+          </div>
+        );
+      });
+      return cats;
+    }else{
+      return (
+        <div></div>
+      );
     }
   }
   componentDidMount() {
     let result = this.props.dispatch(fetchFeed(this.state.user._id,this.state.from,this.state.to));
     result.then((json) => {
-      console.log(json);
+      this.setState({
+        catalogues:json.catalogues
+      });
     });
   }
   render() {
@@ -25,7 +48,11 @@ class Home extends React.Component {
       <div className="container-fluid">
         <Messages messages={this.props.messages}/>
         <div className="row">
-
+          <div className="col-sm-6 col-sm-offset-3">
+            <div className="col-sm-10 col-sm-offset-1">
+              {this.getCatalogues()}
+            </div>
+          </div>
         </div>
       </div>
     );
